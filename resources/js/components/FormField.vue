8e4430b25e8a7f7c74bbdd330c96b23046971b1b
Roberto Negro <negro.roberto2@gmail.com>
2026-03-10 14:30:09 +0100

    build and fix styling

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -28,9 +28,13 @@
           </div>
         </div>
 
-        <DefaultButton type="button" @click.prevent="openModal">
+        <button
+          type="button"
+          class="shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm"
+          @click.prevent="openModal"
+        >
           {{ addButtonText }}
-        </DefaultButton>
+        </button>
 
         <IconPicker
           v-if="modalOpen"
